2df3c89e95ca5becd6f97d7e2b72d10f89ae3008
Omar Baig <omarbaig1998@gmail.com>
2022-02-02 19:34:23 -0600

    Started development on asteroid explosion

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed = 1f;
+    [SerializeField] private GameObject _explosionPrefab = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateAsteroid();
+    }
+
+    /// <summary>
+    /// Rotates the asteroid along the z-axis
+    /// </summary>
+    private void RotateAsteroid()
+    {
+        transform.Rotate(0, 0, _rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Laser"))
+        {
+            Instantiate(_explosionPrefab);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
